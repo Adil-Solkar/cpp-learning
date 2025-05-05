78c654e3c02cb2ab767581beb036913779c05ef4
Adil-Solkar <adilsolkar20@gmail.com>
2025-05-05 19:08:01 +0530

    Variables and constants in C++

---
+++ 01_varconst/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int balance; // Declared an integer variable named 'balance'
+    balance = 500; // Assign the value 500 to the 'balance' variable
+
+    // Declared a constant integer named 'age' with value 100 (cannot be changed)
+    const int age = 100;
+    
+    cout << "learning about variables and constants in cpp";
+    return 0;
+}
